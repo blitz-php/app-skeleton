d5f37035c1ea205324b3181999b3e71f52e5b45d
Dimitri Sitchet Tomkeu <dimitrisitchet@gmail.com>
2023-02-12 08:27:58 +0100

    reorganisation du bootstraping

---
+++ app/Config/paths.php
@@ -0,0 +1,76 @@
+<?php
+
+/**
+ * ------------------------------------------------- -------------------------
+ * Configuration des chemins des differents repertoires de l'application
+ * ------------------------------------------------- -------------------------
+ *
+ * Ce fichier vous permet de definir le chemins où se trouveront les élements nécessaires à l'éxecution du programme
+ * 
+ * Holds the paths that are used by the system to
+ * locate the main directories, app, system, etc.
+ *
+ * Modifying these allows you to restructure your application,
+ * share a system folder between multiple applications, and more.
+ *
+ * All paths are relative to the project's root folder.
+ */
+
+return [
+    /**
+     * ------------------------------------------------- -------------------------
+     * NOM DU DOSSIER D'APPLICATION
+     * ------------------------------------------------- -------------------------
+     *
+     * Repertoire où sont stockés les fichiers de votre application
+     * 
+     * If you want this front controller to use a different "app"
+     * folder than the default one you can set its name here. The folder
+     * can also be renamed or relocated anywhere on your server. If
+     * you do, use a full server path.
+     *
+     * @see http://codeigniter.com/user_guide/general/managing_apps.html
+     * 
+     * @var string
+     */
+    'app' => __DIR__ . '/../',
+    
+    /**
+     * ------------------------------------------------- -------------------------
+     * NOM DU DOSSIER DE STOCKAGE
+     * ---------------------------------------------------------------
+     *
+     * Repertoire où sont stockés les fichiers statique générés par l'application (cache, log, migrations...)
+     * 
+     * This variable must contain the name of your "storage" directory.
+     * The storage directory allows you to group all directories that
+     * need write permission to a single place that can be tucked away
+     * for maximum security, keeping it out of the app and/or
+     * system directories.
+     */
+    'storage' => __DIR__ . '/../../storage/',
+    
+    /**
+     * ------------------------------------------------- -------------------------
+     * NOM DU DOSSIER DES TELECHARGEMENTS
+     * ---------------------------------------------------------------
+     *
+     * Repertoire où sont stockés les fichiers téléchargés par les utilisateurs de l'application (images, videos, pdf...)
+     * 
+     * This variable must contain the name of your "storage" directory.
+     * The storage directory allows you to group all directories that
+     * need write permission to a single place that can be tucked away
+     * for maximum security, keeping it out of the app and/or
+     * system directories.
+     */
+    'upload' => __DIR__ . '/../../uploads/',
+    
+    /**
+     * ------------------------------------------------- -------------------------
+     * NOM DU DOSSIER DE COMPOSER
+     * ---------------------------------------------------------------
+     *
+     * Repertoire votre dossier de dependances installées via composer "vendor".
+     */
+    'composer' => __DIR__ . '/../../vendor/',
+];
--- public/index.php
+++ public/index.php
@@ -1,5 +1,78 @@
 <?php
 
-$app = require_once __DIR__ . DIRECTORY_SEPARATOR . '.bootstrap.php';
+/**
+ * This file is part of Blitz PHP framework.
+ *
+ * (c) 2022 Dimitri Sitchet Tomkeu <devcode.dst@gmail.com>
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
 
-$app->run();
+define('DS', DIRECTORY_SEPARATOR);
+
+// Charge la configuration de nos chemins de repertoires
+// Cette ligne doit etre modifiee en fonction de votre structure
+$paths_config_file = dirname(__DIR__) . '/app/Config/paths.php';
+
+$paths = require_once $paths_config_file;
+
+// Le chemin d'accès vers composer
+if (empty($paths['composer']) || (!is_dir($paths['composer']) && !is_file($paths['composer']))) {
+    header('HTTP/1.1 503 Service Unavailable.', true, 503);
+    echo 'Your composer autoload file does not appear to be set correctly. ';
+    echo 'Please open the following file and correct this: "' . $paths_config_file . '"';
+    exit(3); // EXIT_CONFIG
+}
+
+$paths['composer'] = strtr(rtrim($paths['composer'], '/\\'), '/\\', DS.DS);
+if (is_dir($paths['composer'])) {
+    $paths['composer'] .= DS . 'autoload.php';
+}
+if (!is_file($paths['composer'])) {
+    $paths['composer'] = dirname(__DIR__) . DS . 'vendor' . DS . 'auoload.php';
+}
+if (!is_file($paths['composer'])) {
+    header('HTTP/1.1 503 Service Unavailable.', true, 503);
+    echo 'Your composer autoload file does not appear to be set correctly. ';
+    echo 'Please open the following file and correct this: "' . $paths_config_file . '"';
+    exit(3); // EXIT_CONFIG
+}
+
+require_once $paths['composer'];
+
+/**
+ * Chemin d'acces du dossier "composer"
+ */
+define('COMPOSER_PATH', realpath(pathinfo($paths['composer'], PATHINFO_DIRNAME)) . DS);
+
+/**
+ * Chemin vers le framework
+ */
+define('SYST_PATH', COMPOSER_PATH . 'blitz-php' . DS . 'framework' . DS . 'src' . DS);
+
+/**
+ * Chemin vers le repertoire publique
+ */
+define('WEBROOT', __DIR__  . DS);
+
+/**
+ * URL de base
+ */
+define('BASE_URL', trim(dirname($_SERVER['SCRIPT_NAME'], 2), '\\'));
+
+/**
+ * Chargement du fichier responsable du demarrage du systeme
+ */
+$bootstrap = require_once SYST_PATH . 'Initializer' . DS. 'bootstrap.php';
+
+/**
+ * Lancement de l'application
+ * 
+ * Maintenant que tout est ok, on peut exeecuter l'application
+ */
+$bootstrap(
+    $paths, 
+    $paths_config_file, 
+    PHP_SAPI === 'cli' || defined('STDIN') || defined('KLINGED')
+);
