111a78bb5a245415c07022d65f78b190f6b8feb0
Dimitri Sitchet Tomkeu <dimitrisitchet@gmail.com>
2024-12-16 17:23:34 +0100

    patch: ajout la config pour les fonctionnalites d'optimisation

---
+++ app/Config/optimize.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * -------------------------------
+ * Configuration de l'optimisation
+ * -------------------------------
+ *
+ * @immutable
+ */
+
+return [
+    /**
+     * @see 
+     */
+    'config_cache_enabled' => true,
+    
+    /**
+     * @see 
+     */
+    'locator_cache_enabled' => true,
+];
